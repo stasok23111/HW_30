9856c5e8876e50a7f5ad81416daad4f50a162d42
stasok23111 <116523241+stasok23111@users.noreply.github.com>
2023-05-19 18:04:40 +0300

    Add files via upload

---
+++ ads/serializers.py
@@ -0,0 +1,106 @@
+from rest_framework.fields import SerializerMethodField
+from rest_framework.relations import SlugRelatedField
+from rest_framework.serializers import ModelSerializer
+
+from ads.models import Ad, Category, Selection
+from users.models import User
+
+
+class AdSerializer(ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Ad
+
+
+class AdListSerializer(ModelSerializer):
+    author = SlugRelatedField(slug_field="username", queryset=User.objects.all())
+    category = SlugRelatedField(slug_field="name", queryset=Category.objects.all())
+
+    class Meta:
+        exclude = ('description',)
+        model = Ad
+
+
+class AdAuthorSerializer(ModelSerializer):
+    total_ads = SerializerMethodField()
+
+    def get_total_ads(self, obj):
+        return obj.ad_set.count()
+
+    class Meta:
+        exclude = ('password', 'role')
+        model = User
+
+
+class AdDetailSerializer(ModelSerializer):
+    author = AdAuthorSerializer()
+    category = SlugRelatedField(slug_field="name", queryset=Category.objects.all())
+
+    class Meta:
+        fields = '__all__'
+        model = Ad
+
+
+class CategoryListSerializer(ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Category
+
+
+class CategoryCreateSerializer(ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Ad
+
+
+class CategoryUpdateSerializer(ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Ad
+
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Category
+
+
+class SelectionSerializer(ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Selection
+
+class SelectionCreateSerializer(ModelSerializer):
+    owner = SlugRelatedField(slug_field='username', required=False, read_only=True)
+
+    def create(self, validated_data):
+
+        request = self.context.get('request')
+        validated_data['owner'] = request.user
+
+
+        return super().create(validated_data)
+    class Meta:
+        fields = '__all__'
+        model = Selection
+
+
+class SelectionListSerializer(ModelSerializer):
+    class Meta:
+        fields = ['pk', 'name']
+        model = Selection
+
+
+class SelectionDetailSerializer(ModelSerializer):
+
+    items = AdSerializer(many=True)
+    owner = SlugRelatedField(slug_field='username', queryset=User.objects.all())
+
+
+    class Meta:
+        fields = '__all__'
+        model = Selection
+
+
+
+
